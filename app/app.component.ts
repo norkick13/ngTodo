d708ba39c90641eeada699bd1f4b3a07430e0828
Norvinder Singh <norvinder@gmail.com>
2018-05-07 15:41:36 -0400

    To Do App

---
+++ app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, Class } from "@angular/core";
+import { Model, TodoItem } from "./model";
+
+@Component({
+    selector: "toDo-app",
+    templateUrl: "app/app.component.html"
+})
+
+export class AppComponent {
+    model= new Model();
+    getName() {
+        return this.model.user;
+    }
+    getTodoItems() {
+        return this.model.items.filter(item => !item.done);
+    }
+    addItem(newItem) {
+        if (newItem !=""){
+            this.model.items.push(new TodoItem(newItem,false));
+        }
+    }
+}
